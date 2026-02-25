f2f937c9ece233b2cd213b42296f36d3bea8d002
Sahil Suman <sahilsuman1202@gmail.com>
2026-02-25 23:17:05 +0530

    feat: add TRPC initialization with context and OpenAPI metadata

---
+++ src/server/trpc.ts
@@ -0,0 +1,8 @@
+import {initTRPC} from '@trpc/server';
+import { Context } from './context';
+import {OpenApiMeta} from 'trpc-to-openapi';
+
+const t = initTRPC.context<Context>().meta<OpenApiMeta>().create();
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
